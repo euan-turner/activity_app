301990384fd74890590195aa3736e9fe766dfff4
Euan Turner <euanoturner@gmail.com>
2022-07-15 12:56:03 +0100

    Added gpx data retrieval, with conversion to string

--- src/app/api.py
+++ src/app/api.py
@@ -6,7 +6,7 @@ from garminconnect import (
 )
 
 def get_csv_data(api : Garmin, activity_id : str):
-    """Gets the csv data for an activity id from Garmin Connect API
+    """Gets the csv data for an activity from Garmin Connect API
 
     Args:
         api (Garmin): API instance from garminconnect
@@ -17,6 +17,19 @@ def get_csv_data(api : Garmin, activity_id : str):
     """    
     csv_data = api.download_activity(activity_id, dl_fmt = api.ActivityDownloadFormat.CSV)
     return csv_data
+
+def get_gpx_data(api : Garmin, activity_id : str):
+    """Gets the gpx data for an activity from Garmin Connect API
+
+    Args:
+        api (Garmin): API instance from garminconnect
+        activity_id (str): Activity ID
+
+    Returns:
+        str: Activity data in gpx format
+    """    
+    gpx_data = api.download_activity(activity_id, dl_fmt = api.ActivityDownloadFormat.GPX)
+    return gpx_data.decode(encoding = 'utf-8')
 
 def save_data(data, activity_id : str, suffix : str):  
     """Saves activity data to a file
@@ -38,5 +51,6 @@ api.login()
 activities = api.get_activities_by_date('2022-05-02', '2022-05-02', 'running')
 for activity in activities:
     activity_id = activity["activityId"]
-    data = get_csv_data(api, activity_id)
+    data = get_gpx_data(api, activity_id)
+    print(data)
 
